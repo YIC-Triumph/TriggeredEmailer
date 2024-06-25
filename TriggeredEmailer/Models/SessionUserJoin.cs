65b347a3547c6e8fd04b2d683179108cce054cdf
jshprz <joshuaperezmelgazo@gmail.com>
2024-06-26 02:42:01 +0800

    Retrieval of incomplete sessions

--- TriggeredEmailer/Models/SessionUserJoin.cs
+++ TriggeredEmailer/Models/SessionUserJoin.cs
@@ -11,11 +11,20 @@ namespace TriggeredEmailer.Models
         public int? sessID { get; set; }
         public int? TherapistID { get; set; }
         public int? Affirmed { get; set; }
+        public string? notes { get; set; }
         public string? AbsenceReason { get; set; }
+        public string? starttime { get; set; }
+        public string? endtime { get; set; }
+        public decimal? SessHrs { get; set; }
+        public DateTime? sessdate { get; set; }
+        public string? StudentFirstName { get; set; }
+        public string? StudentLastName { get; set; }
 
         // Properties from users
         public int? UserID { get; set; }
         public string? UserName { get; set; }
         public string? EmailAddress { get; set; }
+        public string? ProviderFirstName { get; set; }
+        public string? ProviderLastName { get; set; }
     }
 }
--- TriggeredEmailer/Services/SessionService.cs
+++ TriggeredEmailer/Services/SessionService.cs
@@ -29,15 +29,28 @@ namespace TriggeredEmailer.Services
 
         public async Task SendEmailToProviderForIncompleteSessionAsync()
         {
+            string technicalSupportEmail = "Technicalsupport@triumphaba.com";
             var incompleteScheduledSessionResult = _dbContext.sessionUserJoins
-                .FromSqlRaw("SELECT s.sessID, s.TherapistID, s.Affirmed, s.AbsenceReason, u.UserID, u.UserName, u.EmailAddress FROM vwSessions s INNER JOIN users u ON s.TherapistID = u.UserID")
-                .Where(s => s.Affirmed == 0)
-                .Where(s => s.AbsenceReason == null || s.AbsenceReason.Length < 1)
+                .FromSqlRaw("SELECT s.sessID, s.TherapistID, s.Affirmed, s.notes, s.AbsenceReason, s.starttime, s.endtime, s.SessHrs, s.sessdate, s.FirstName As StudentFirstName, s.LastName As StudentLastName, u.UserID, u.UserName, u.EmailAddress, u.FirstName As ProviderFirstName, u.LastName As ProviderLastName FROM vwSessions s INNER JOIN vwStaff u ON s.TherapistID = u.LoginID WHERE s.SessionStatus NOT IN (3, 4)")
+                .Where(s => s.notes == "")
+                .Where(s => s.notes == null)
+                .Where(s => s.sessdate < DateTime.Now)
                 .ToList();
-
+            
             foreach (var session in incompleteScheduledSessionResult)
             {
-                var message = $"Dear {session.UserName},\n\nYou have an incomplete session scheduled for this session id: {session.sessID}.\n\nKindly edit the incomplete scheduled session through this link: {_appSettings.Domain}SessionEdit.aspx?SS_ID={session.sessID}&ShowAll=False&QuickAdd=False&returnTo=caseload-clientlist&Staff_ID={session.TherapistID}&AllAssmts= \n\nThank you,\n\nTriumph";
+                string editSessionLink = $"{_appSettings.Domain}SessionEdit.aspx?SS_ID={session.sessID}&ShowAll=False&QuickAdd=False&returnTo=caseload-clientlist&Staff_ID={session.TherapistID}&AllAssmts=";
+
+                var message = $@"
+                    Dear {session.ProviderFirstName} {session.ProviderLastName},
+
+                    Your session with {session.StudentFirstName} {session.StudentLastName} from {session.starttime} to {session.endtime} has ended.
+
+                    <a href=""{editSessionLink}"">Click on this link to enter in and complete your session</a>.
+                    
+                    If you have any questions, feel free to reach out to {technicalSupportEmail} x613 for assistance.
+                    
+                    - Triumph Behavior Support.";
 
                 await _emailService.SendEmail(session.EmailAddress, "Incomplete Session", message);
             }
